33a61bcbecdc91aa078b54a4214f72ebb1dfc46c
Kuliak Andrii <andrii.kuliak@gmail.com>
2020-12-16 17:02:37 +0200

    general: add basic logging support
    Signed-off-by: Andrii Kuliak <andrii.kuliak@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-import os
 import telebot
+import logging
 
 from security import GoogleCloudSecurityProxy
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s - %(message)s')
 
 sec_manager_proxy = GoogleCloudSecurityProxy()
 
@@ -9,10 +11,14 @@ bot_proxy = telebot.TeleBot(sec_manager_proxy.get_bot_auth_token())
 
 @bot_proxy.message_handler(commands=['start'])
 def initiate_session(message):
+    logging.debug("Handling session initialisation from %s(id: %d)", message.from_user.username, message.from_user.id)
     bot_proxy.send_message(message.chat.id, 'Hello, fellow!')
 
 @bot_proxy.message_handler(content_types=['text'])
 def handle_user_input(message):
+    logging.debug("Handling user text input from %s(id: %d)", message.from_user.username, message.from_user.id)
     bot_proxy.send_message(message.chat.id, message.text)
 
-bot_proxy.polling()
+if __name__ == "__main__":
+    logging.info('Starting nessage polling from Telegram servers')
+    bot_proxy.polling()
--- src/security.py
+++ src/security.py
@@ -1,15 +1,16 @@
+import logging
 from abc import ABC, abstractmethod
 
 from google.cloud import secretmanager
 
 class SecurityManagerProxyBase(ABC):
-
     @abstractmethod
     def get_bot_auth_token(self):
         raise NotImplementedError
 
 class GoogleCloudSecurityProxy(SecurityManagerProxyBase):
     def __init__(self):
+        logging.debug("GoogleCloudSecurityProxy cretion")
         super().__init__()
         self._sec_mgr_proxy = secretmanager.SecretManagerServiceClient()
 
@@ -21,6 +22,9 @@ class GoogleCloudSecurityProxy(SecurityManagerProxyBase):
     def _get_secret_info(self, project_id, secret_id):
         secret_full_name = "projects/{}/secrets/{}/versions/latest".format(project_id, secret_id)
 
+        logging.info("Requesting secret '{}'".format(secret_full_name))
         secret_info = self._sec_mgr_proxy.access_secret_version(request={"name": secret_full_name})
+        logging.info("Successfully requested secret")
+
         return secret_info.payload
 
